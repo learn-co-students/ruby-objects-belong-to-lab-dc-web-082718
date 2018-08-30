74583e2802bb1ddf2af1af502fc7c9a1ae8a3a8b
pasolre95kim <pasolre95kim@gmail.com>
2018-08-30 15:11:30 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+require "pry"
+require_relative "./artist.rb"
+class Song
+
+  # def title
+  #   @title
+  # end
+  #
+  # def title=(title)
+  #   @title=title
+  # end
+
+  attr_accessor :title, :artist, :song
+
+end
